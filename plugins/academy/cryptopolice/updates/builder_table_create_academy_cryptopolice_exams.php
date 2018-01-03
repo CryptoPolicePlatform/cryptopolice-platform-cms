899a1cfa731979168f628c6aac070424b5b839e7
Vladimirs <vladimirs@cunami.lv>
2018-01-03 09:59:33 +0200

    Creating a database tables

---
+++ plugins/academy/cryptopolice/updates/builder_table_create_academy_cryptopolice_exams.php
@@ -0,0 +1,31 @@
+<?php namespace Academy\CryptoPolice\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateAcademyCryptopoliceExams extends Migration
+{
+    public function up()
+    {
+        Schema::create('academy_cryptopolice_exams', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id')->unsigned();
+            $table->string('exam_title', 255);
+            $table->string('exam_description', 255);
+            $table->string('exam_slug', 255);
+            $table->text('question');
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+            $table->timestamp('deleted_at')->nullable();
+            $table->integer('timer');
+            $table->integer('s_score');
+            $table->integer('status');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('academy_cryptopolice_exams');
+    }
+}
