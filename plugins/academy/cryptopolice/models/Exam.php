3239e81ac02ab80cf4e589de8e24602bac238844
Vladimirs <vladimirs@cunami.lv>
2018-01-03 10:49:00 +0200

    Exam model

---
+++ plugins/academy/cryptopolice/models/Exam.php
@@ -0,0 +1,26 @@
+<?php namespace Academy\CryptoPolice\Models;
+
+use Model;
+
+/**
+ * Model
+ */
+class Exam extends Model
+{
+    use \October\Rain\Database\Traits\Validation;
+    
+    use \October\Rain\Database\Traits\SoftDelete;
+
+    protected $dates = ['deleted_at'];
+
+    /**
+     * @var array Validation rules
+     */
+    public $rules = [
+    ];
+
+    /**
+     * @var string The database table used by the model.
+     */
+    public $table = 'academy_cryptopolice_exams';
+}
