1a298d3c85d1141087d37ef01ad01736e1290156
Vladimirs <vladimirs@cunami.lv>
2018-01-03 15:16:05 +0200

    Plugin component registration

--- plugins/academy/cryptopolice/Plugin.php
+++ plugins/academy/cryptopolice/Plugin.php
@@ -5,7 +5,11 @@ use System\Classes\PluginBase;
 class Plugin extends PluginBase
 {
     public function registerComponents()
-    {
+    { 
+	return [
+            'Cryptopolice\NewAcademy\Components\Exams' => 'Exams',
+            'Cryptopolice\NewAcademy\Components\ExamTask' => 'ExamTask'
+        ];
     }
 
     public function registerSettings()
