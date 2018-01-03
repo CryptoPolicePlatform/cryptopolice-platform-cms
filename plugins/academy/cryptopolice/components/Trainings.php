e67f509122e93e0fe6f9c0acfe2ce07e2cdacb1a
Vladimirs <vladimirs@cunami.lv>
2018-01-03 15:59:53 +0200

    Training Task & Training List components

---
+++ plugins/academy/cryptopolice/components/Trainings.php
@@ -0,0 +1,52 @@
+<?php namespace CryptoPolice\NewAcademy\Components;
+
+use Auth;
+use Redirect;
+use Response;
+use Cms\Classes\ComponentBase;
+use CryptoPolice\newAcademy\Models\Training;
+
+class Trainings extends ComponentBase
+{
+
+    public function componentDetails()
+    {
+        return [
+            'name'        => 'Training List',
+            'description' => 'Training List of tasks.'
+        ];
+    }
+
+    public function defineProperties()
+    {
+        return [
+            'max' => [
+                'description'       => 'The most amount of todo items allowed',
+                'title'             => 'Max items',
+                'default'           => 10,
+                'type'              => 'string',
+                'validationPattern' => '^[0-9]+$',
+                'validationMessage' => 'The Max Items value is required and should be integer.'
+            ]
+        ];
+    }
+
+    public $trainings;
+
+    public function onRun() {
+
+        $loggedIn = Auth::check();
+        if(!$loggedIn) {
+            return Redirect::to('/login');
+        }
+
+        $training = Training::paginate(10);
+       
+        if(!$training) {
+           return $this->controller->run('404');
+        } else {
+            $this->trainings = $training;
+        }
+    }
+
+}
