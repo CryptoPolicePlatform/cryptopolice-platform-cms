ff9ab851557a124a30c656adbc4d021a798f68a9
igors savins <igors@cunami.lv>
2018-01-11 18:15:35 +0200

    beforeRegister event validation

--- plugins/academy/cryptopolice/Plugin.php
+++ plugins/academy/cryptopolice/Plugin.php
@@ -1,6 +1,12 @@
 <?php namespace Academy\CryptoPolice;
 
+use Event;
+use ApplicationException;
 use System\Classes\PluginBase;
+use RainLab\User\Models\User as UserModel;
+use RainLab\User\Controllers\Users as UsersController;
+
+
 
 class Plugin extends PluginBase
 {
@@ -11,11 +17,28 @@ class Plugin extends PluginBase
             'Academy\Cryptopolice\Components\ExamTask' => 'ExamTask',
             'Academy\Cryptopolice\Components\Trainings' => 'Trainings',
             'Academy\Cryptopolice\Components\ProfileForm' => 'ProfileForm',
-            'Academy\Cryptopolice\Components\TrainingTask' => 'TrainingTask'
+            'Academy\Cryptopolice\Components\TrainingTask' => 'TrainingTask',
         ];
     }
 
     public function registerSettings()
     {
+    }
+
+    public function boot()
+    {
+        Event::listen('rainlab.user.beforeRegister', function($user) {
+
+            $userPassword = post('password');
+
+            if(!preg_match('/[a-zA-Z]/',$userPassword)) {
+                throw new ApplicationException('Password should contain at least one letter character');
+            }
+
+            if(!preg_match('/[^a-zA-Z\d]/',$userPassword)) {
+                throw new ApplicationException('Password should contain at least one special character');
+            }
+
+        });
     }
 }
