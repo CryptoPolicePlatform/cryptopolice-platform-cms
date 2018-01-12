9391238f0e93bb1d5e482622e25d268aaa6299f8
igors savins <igors@cunami.lv>
2018-01-12 16:47:49 +0200

    Update link & file upload

--- plugins/academy/cryptopolice/components/customUploader.php
+++ plugins/academy/cryptopolice/components/customUploader.php
@@ -14,27 +14,26 @@ use Cms\Classes\ComponentBase;
 class customUploader extends ComponentBase
 {
 
-    public function init()
-    {
-
-        $user = Auth::getUser();
-
-        if ($user) {
-            $component = $this->addComponent(
-                'NetSTI\Uploader\Components\ImageUploader',
-                'customUploader',
-                ['modelClass' => 'RainLab\User\Models\User', 'modelKeyColumn' => 'avatar', 'deferredBinding' => false]
-            );
-            $component->bindModel('avatar', $user);
-        }
-    }
-
     public function componentDetails()
     {
         return [
             'name' => 'Custom Image Uploader',
             'description' => 'Custom Image Uploader'
         ];
+    }
+
+    function init()
+    {
+        $user = Auth::getUser();
+        if($user){
+            $component = $this->addComponent(
+                'NetSTI\Uploader\Components\ImageUploader',
+                'customUploader',
+                ['modelClass'=>'RainLab\User\Models\User','modelKeyColumn'=>'avatar', 'deferredBinding' => false]
+            );
+
+            $component->bindModel('avatar', $user);
+        }
     }
 
 }
