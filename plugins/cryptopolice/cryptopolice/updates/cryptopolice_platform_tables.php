5966ed5f816725564e7532b80cc527dcb3732d14
ART <art@cunami.lv>
2018-01-14 23:20:22 +0200

    Update migration and add category table

--- plugins/cryptopolice/cryptopolice/updates/cryptopolice_platform_tables.php
+++ plugins/cryptopolice/cryptopolice/updates/cryptopolice_platform_tables.php
@@ -39,22 +39,12 @@ class CryptopolicPlatformTables extends Migration {
             $table->timestamp('deleted_at')->nullable();
             $table->dateTime('completed_at');
             $table->boolean('complete_status')->default(0);
-            $table->integer('try')->default(0);
-        });
-
-        Schema::create('cryptopolice_cryptopolice_final_exam_score', function ($table) {
-            $table->engine = 'InnoDB';
-            $table->increments('id');
-            $table->timestamp('created_at')->nullable();
-            $table->timestamp('updated_at')->nullable();
-            $table->timestamp('deleted_at')->nullable();
-            $table->integer('exam_id');
             $table->integer('question_num');
             $table->integer('answer_num');
             $table->boolean('is_correct');
-            $table->integer('user_id');
             $table->integer('try')->default(0);
         });
+
 
         Schema::create('cryptopolice_cryptopolice_trainings', function ($table) {
             $table->engine = 'InnoDB';
@@ -68,6 +58,19 @@ class CryptopolicPlatformTables extends Migration {
             $table->integer('user_id')->default(0);
             $table->integer('status')->default(0);
             $table->integer('likes')->default(0);
+        });
+
+        Schema::create('cryptopolice_cryptopolice_trainings_category', function ($table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->text('title')->nullable();
+            $table->text('slug')->nullable();
+            $table->text('description')->nullable();
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+            $table->timestamp('deleted_at')->nullable();
+            $table->integer('user_id')->default(0);
+            $table->integer('status')->default(0);
         });
 
         if (Schema::hasColumns('users', ['eth_address'])) {
