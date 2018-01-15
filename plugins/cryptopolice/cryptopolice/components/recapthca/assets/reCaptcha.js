45ac5e272f8dc304e29c2d83aa44e107719149a6
Vladimirs <vladimirs@cunami.lv>
2018-01-15 18:30:55 +0200

    reCaptcha update

--- plugins/cryptopolice/cryptopolice/components/recapthca/assets/reCaptcha.js
+++ plugins/cryptopolice/cryptopolice/components/recapthca/assets/reCaptcha.js
@@ -3,7 +3,7 @@ var onloadCallback = function () {
     $(".g-recaptcha").each(function () {
         var el = $(this);
         var captcha = grecaptcha.render($(el).attr("id"), {
-            "sitekey": '6Ld4s0AUAAAAAKwVMVLLOuPt6xUcsQd1a_-bQTtJ',
+            "sitekey": '6Lfux0AUAAAAAFNtyFNr0VBkfI-sZfbHgkjcuzPv',
             "callback": function (token) {
                 $(el).parent().find(".g-recaptcha-response").val(token);
                 $(el).parent().submit();
