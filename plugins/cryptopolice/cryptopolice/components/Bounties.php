f650d6e9588c85b4b9b3539d583bed97a402d792
Vladimirs <vladimirs@cunami.lv>
2018-01-18 14:01:31 +0200

    Bounty Model update

---
+++ plugins/cryptopolice/cryptopolice/components/Bounties.php
@@ -0,0 +1,24 @@
+<?php namespace CryptoPolice\CryptoPolice\Components;
+
+use Cms\Classes\ComponentBase;
+use CryptoPolice\CryptoPolice\Models\Bounty;
+
+class Bounties extends ComponentBase
+{
+
+    public $bounty;
+
+    public function componentDetails()
+    {
+        return [
+            'name' => 'Bounty list',
+            'description' => 'Bounty Campaign List'
+        ];
+    }
+
+    public function onRun()
+    {
+        $this->bounty = Bounty::get();
+    }
+
+}
---
+++ plugins/cryptopolice/cryptopolice/controllers/Bounties.php
@@ -0,0 +1,23 @@
+<?php namespace CryptoPolice\CryptoPolice\Controllers;
+
+use Backend\Classes\Controller;
+use BackendMenu;
+
+class Bounties extends Controller
+{
+    public $implement = [
+        'Backend\Behaviors\ListController',
+        'Backend\Behaviors\FormController',
+        'Backend\Behaviors\ReorderController'
+    ];
+
+    public $listConfig = 'config_list.yaml';
+    public $formConfig = 'config_form.yaml';
+    public $reorderConfig = 'config_reorder.yaml';
+
+    public function __construct()
+    {
+        parent::__construct();
+        BackendMenu::setContext('CryptoPolice.CryptoPolice', 'main-menu-item5');
+    }
+}
