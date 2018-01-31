fd9535a672d80882a043d1ee76b72de34c7752a0
Vladimirs <vladimirs@cunami.lv>
2018-01-31 19:54:12 +0200

    Custom formWidget for registrationField

---
+++ plugins/cryptopolice/bounty/formwidgets/Registrationbox.php
@@ -0,0 +1,25 @@
+<?php namespace Cryptopolice\Bounty\FormWidgets;
+
+use Cryptopolice\Bounty\Models\BountyRegistration;
+use Backend\Classes\FormWidgetBase;
+use Config;
+
+class RegistrationBox extends FormWidgetBase
+{
+    public function widgetDetails()
+    {
+        return [
+            'name' => 'Registrationbox',
+            'description' => 'Field for registration data'
+        ];
+    }
+
+    public function render(){
+        $this->prepareVars();
+        return $this->makePartial('widget');
+    }
+
+    public function prepareVars() {
+        $this->vars['registrationData'] = BountyRegistration::where('id', $this->model->bounty_user_registration_id)->value('fields_data');
+    }
+}
