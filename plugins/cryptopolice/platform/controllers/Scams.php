8978377f2e74829c88f4096196fbce6d96f59e75
Vladimirs <vladimirs@cunami.lv>
2018-02-19 10:25:20 +0200

    Scam model & controller

---
+++ plugins/cryptopolice/platform/controllers/Scams.php
@@ -0,0 +1,18 @@
+<?php namespace CryptoPolice\Platform\Controllers;
+
+use Backend\Classes\Controller;
+use BackendMenu;
+
+class Scams extends Controller
+{
+    public $implement = [        'Backend\Behaviors\ListController',        'Backend\Behaviors\FormController'    ];
+    
+    public $listConfig = 'config_list.yaml';
+    public $formConfig = 'config_form.yaml';
+
+    public function __construct()
+    {
+        parent::__construct();
+        BackendMenu::setContext('CryptoPolice.Platform', 'scams');
+    }
+}
--- plugins/cryptopolice/platform/updates/cryptopolice_platform_tables.php
+++ plugins/cryptopolice/platform/updates/cryptopolice_platform_tables.php
@@ -100,10 +100,24 @@ class BuilderTableCreateCryptopolicePlatformCommunityPosts extends Migration
                 $table->timestamp('deleted_at')->nullable();
             });
         }
+
+        if (!Schema::hasTable('cryptopolice_platform_scams')) {
+
+            Schema::create('cryptopolice_platform_scams', function ($table) {
+                $table->engine = 'InnoDB';
+                $table->increments('id');
+                $table->integer('user_id')->nullable();
+                $table->boolean('status')->default(0);
+                $table->string('title', 255)->nullable();
+                $table->string('description', 1000)->nullable();
+                $table->timestamp('created_at')->nullable();
+                $table->timestamp('updated_at')->nullable();
+                $table->timestamp('deleted_at')->nullable();
+            });
+        }
     }
 
-    public function down()
-    {
+    public function down() {
 
     }
 }
