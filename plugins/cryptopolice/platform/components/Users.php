8dcc0fbc5960d54687662977923499f24ef42474
Vladimirs <vladimirs@cunami.lv>
2018-02-21 20:51:10 +0200

    Update users sidebar

--- plugins/cryptopolice/platform/components/Users.php
+++ plugins/cryptopolice/platform/components/Users.php
@@ -1,6 +1,7 @@
 <?php namespace CryptoPolice\Platform\Components;
 
 use DB;
+use RainLab\User\Models\User;
 use Cms\Classes\ComponentBase;
 use CryptoPolice\Platform\Classes\Helpers;
 
@@ -18,24 +19,12 @@ class Users extends ComponentBase
     public function onRun()
     {
 
-        $users = Db::table('users')
-            ->leftJoin('system_files', function ($join) {
-                $join->on('users.id', '=', 'system_files.attachment_id')
-                    ->where('system_files.attachment_type', 'RainLab\User\Models\User');
-            })
-            ->select('system_files.disk_name as user_image', 'users.*')
+        $users = User::with('avatar')
             ->whereNotNull('users.last_seen')
             ->orderBy('last_seen', 'desc')
             ->get();
 
         $helper = new Helpers();
-
-        foreach ($users as $key => $value) {
-            if ($value->user_image) {
-                $users[$key]->user_image = $helper->setImagePath($value->user_image);
-            }
-        }
-
         $this->page['users'] = $users;
     }
 }
