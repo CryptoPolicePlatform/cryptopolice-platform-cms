f0d0448951613c8d606b098fa4f77fc536fc92d2
Vladimirs <vladimirs@cunami.lv>
2018-05-11 19:41:25 +0300

    Report check fix

--- plugins/cryptopolice/bounty/components/ReportDetails.php
+++ plugins/cryptopolice/bounty/components/ReportDetails.php
@@ -20,19 +20,20 @@ class ReportDetails extends ComponentBase
     {
         $user = Auth::getUser();
 
-        $report = BountyReport::with('user', 'reward')->select('cryptopolice_bounty_user_reports.id as report_id', 'cryptopolice_bounty_user_reports.*', 'users.*')
-            ->join('users', 'users.id', '=', 'cryptopolice_bounty_user_reports.user_id')
-            ->join('cryptopolice_bounty_campaigns', 'cryptopolice_bounty_campaigns.id', '=', 'cryptopolice_bounty_user_reports.bounty_campaigns_id')
-            ->where('cryptopolice_bounty_user_reports.id', $this->param('id'))
-            ->Where(function ($query) use ($user) {
-                if (!$user->is_superuser) {
-                    $query->where('user_id', $user->id)
-                        ->orWhere('cryptopolice_bounty_campaigns.id', 3)
-                        ->where('cryptopolice_bounty_user_reports.report_status', 1);
-                }
-            })
-            ->first();
-
+        if(isset($user) & !empty($user)) {
+            $report = BountyReport::with('user', 'reward')->select('cryptopolice_bounty_user_reports.id as report_id', 'cryptopolice_bounty_user_reports.*', 'users.*')
+                ->join('users', 'users.id', '=', 'cryptopolice_bounty_user_reports.user_id')
+                ->join('cryptopolice_bounty_campaigns', 'cryptopolice_bounty_campaigns.id', '=', 'cryptopolice_bounty_user_reports.bounty_campaigns_id')
+                ->where('cryptopolice_bounty_user_reports.id', $this->param('id'))
+                ->Where(function ($query) use ($user) {
+                    if (!$user->is_superuser) {
+                        $query->where('user_id', $user->id)
+                            ->orWhere('cryptopolice_bounty_campaigns.id', 3)
+                            ->where('cryptopolice_bounty_user_reports.report_status', 1);
+                    }
+                })
+                ->first();
+        }
         if (!empty($report)) {
             $this->page['report'] = $report;
         } else {
